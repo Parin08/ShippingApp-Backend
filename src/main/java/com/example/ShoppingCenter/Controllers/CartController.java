218a7ae7a0ff7bd3a86612851f9e057f9707fd48
parin <pate0468@algonquinlive.com>
2023-05-02 14:35:48 -0400

    update cart

--- src/main/java/com/example/ShoppingCenter/Controllers/CartController.java
+++ src/main/java/com/example/ShoppingCenter/Controllers/CartController.java
@@ -20,7 +20,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000/", allowCredentials = "true")
+@CrossOrigin(origins = "*", allowCredentials = "true")
 
 public class CartController {
 	@Autowired
